782bd088ab43394184a036e70e35db729f3344e9
Jared <jared@jsorge.net>
2013-10-08 19:09:34 -0700

    Created a fresh project as a document-based application.

---
+++ Meeting Tracker/Meeting Tracker/JMSDocument.h
@@ -0,0 +1,13 @@
+//
+//  JMSDocument.h
+//  Meeting Tracker
+//
+//  Created by Jared Sorge on 10/8/13.
+//  Copyright (c) 2013 jsorge. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JMSDocument : NSDocument
+
+@end
